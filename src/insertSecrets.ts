19722593e93adf236602ee193fa2df87ffd4ab69
Ch1py7 <garcialopezgerardo6612@gmail.com>
2023-07-23 23:34:04 -0600

    feat: add id as a new objectid

--- src/insertSecrets.ts
+++ src/insertSecrets.ts
@@ -1,11 +1,14 @@
 import express, { Router } from 'express'
 import { client } from './client'
+import { ObjectId } from 'mongodb'
+
+const id = new ObjectId()
 
 const insertSecrets = async (req: express.Request, res: express.Response) => {
   try {
-    const { name, age, tags, genre, secret } = req.query
+    const { name, age, tags, gender, secret } = req.query
 
-    const dataToInsert = { name: name, age: age, tags: tags, genre: genre, secret: secret }
+    const dataToInsert = { name: name, age: age, tags: tags, gender: gender, secret: secret, id: id }
 
     await client.connect()
     const database = client.db('my_secrets')

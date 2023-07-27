9eb75c91101ecaa5a6e21458339700c166214338
Ch1py7 <garcialopezgerardo6612@gmail.com>
2023-07-27 01:28:22 -0600

    feat: add pagination for secrets

--- src/secrets.ts
+++ src/secrets.ts
@@ -2,13 +2,23 @@ import express, { Router } from 'express'
 import { client } from './client'
 
 const getSecrets = async (req: express.Request, res: express.Response) => {
+  const { page, pageSize } = req.query
+
+  const pageNumber = parseInt(page as string, 10) || 1
+  const limit = parseInt(pageSize as string, 10) || 10
+  const skip = (pageNumber - 1) * limit
+
+  if (pageNumber < 1 || limit < 1) {
+    return res.status(400).send({ error: 'Invalid pagination parameters' })
+  }
+  
   try {
     await client.connect()
     await client.db('admin').command({ ping: 1 })
     const database = client.db('my_secrets')
     const collection = database.collection('secrets')
 
-    const findResult = await collection.find({}).toArray()
+    const findResult = await collection.find().skip(skip).limit(limit).toArray()
 
     res.send(findResult)
   // eslint-disable-next-line @typescript-eslint/no-explicit-any

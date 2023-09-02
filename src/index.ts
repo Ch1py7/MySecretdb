e825d6fdd252d5a6d3a6dc1130e9b4fd2e23ca55
Ch1py7 <garcialopezgerardo6612@gmail.com>
2023-09-02 01:36:46 -0600

    feat: add entry point

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,21 @@
+import 'module-alias/register'
 import dotenv from 'dotenv'
 dotenv.config()
-import express from 'express'
-import secrets from './secrets'
-import insertSecrets from './insertSecrets'
-import updateLikes from './updateLikes'
-import updateDislikes from './updateDislikes'
-import allSecrets from './allSecrets'
 import cors from 'cors'
+import express from 'express'
+import { router as secretRoutes } from './infrastructure/http/secret-controller'
 
 const app = express()
+const port = process.env.PORT || 8000
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/api/health', (_, res) => {
-  res.send({ status: 'ok' })
-})
+app.use('/api/v1/secret', secretRoutes)
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`)
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
 })
-
-app.use('/api/allsecrets', allSecrets)
-app.use('/api/secrets', secrets)
-app.use('/api/insert', insertSecrets)
-app.use('/api/updatelikes', updateLikes)
-app.use('/api/updatedislikes', updateDislikes)
 
 export default app

3fd596e5bed69905b75db68c219b9211e13b1435
Ch1py7 <garcialopezgerardo6612@gmail.com>
2023-09-08 12:19:42 -0600

    feat: add types and values/classes to container

--- src/container.ts
+++ src/container.ts
@@ -2,9 +2,13 @@ import { InjectionMode, asClass, asFunction, createContainer, asValue } from 'aw
 import { SaveSecret } from 'application/save_secret'
 import { DbHandler } from 'infrastructure/persistance/mongo/db-handler'
 import { MongoSecretRepository } from 'infrastructure/persistance/mongo/mongo-secret-repository'
-import { secretParser } from './infrastructure/persistance/mongo/secret-document-parser'
-import { UpdateLike } from './application/update_likes'
+import { secretParser } from 'infrastructure/persistance/mongo/secret-document-parser'
+import { UpdateLike } from 'application/update_likes'
 import * as mongodb from 'mongodb'
+import { GetSecrets } from 'application/get_secrets'
+import { IdGenerator } from 'domain/secret/services/id-generator'
+import * as crypto from 'crypto'
+import { from } from 'uuid-mongodb'
 
 const container = createContainer<Dependencies>({
   injectionMode: InjectionMode.PROXY,
@@ -16,7 +20,11 @@ container.register({
   saveSecret: asClass(SaveSecret),
   secretRepository: asClass(MongoSecretRepository),
   updateLike: asClass(UpdateLike),
-  mongodb: asValue(mongodb)
+  mongodb: asValue(mongodb),
+  getSecrets: asClass(GetSecrets),
+  idGenerator: asClass(IdGenerator),
+  crypto: asValue(crypto),
+  from: asValue(from)
 })
 
 export default container
--- src/types/container.d.ts
+++ src/types/container.d.ts
@@ -1,11 +1,15 @@
+import { GetSecrets } from 'application/get_secrets'
 import { SaveSecret } from 'application/save_secret'
-import { MongoSecretRepository } from 'infrastructure/persistance/mongo/mongo-secret-repository'
-import { DbHandler } from 'infrastructure/types/database'
-import { Secret } from 'domain/secret/Secret'
-import { SecretDocumentParser } from 'infrastructure/types/document-parser'
 import { UpdateLike } from 'application/update_likes'
+import * as crypto from 'crypto'
+import { Secret } from 'domain/secret/Secret'
+import { IdGenerator } from 'domain/secret/services/id-generator'
+import { MongoSecretRepository } from 'infrastructure/persistance/mongo/mongo-secret-repository'
+import { Collection, DbHandler } from 'infrastructure/types/database'
+import { SecretDocumentParser } from 'infrastructure/types/document-parser'
 import * as mongodb from 'mongodb'
-
+import { MUUID, from } from 'uuid-mongodb'
+  
 declare global {
   interface Dependencies {
     saveSecret: SaveSecret
@@ -15,5 +19,11 @@ declare global {
     secretDocumentParser: SecretDocumentParser
     updateLike: UpdateLike
     mongodb: typeof mongodb
+    getSecrets: GetSecrets
+    idGenerator: IdGenerator
+    crypto: typeof crypto
+    collection: Collection
+    from: typeof from
+    muuid: MUUID
   }
 }

9497306da0c7d284aad1aa307c33e8d11c4fffc3
Ch1py7 <garcialopezgerardo6612@gmail.com>
2023-09-12 23:51:20 -0600

    feat: change type of parser constructor

--- src/infrastructure/persistance/mongo/secret-document-parser.ts
+++ src/infrastructure/persistance/mongo/secret-document-parser.ts
@@ -2,37 +2,10 @@ import { Secret } from 'domain/secret/Secret'
 import { DocumentParser } from 'infrastructure/types/document-parser'
 import { SecretEntity } from 'src/domain/types/Secret'
 
-// export const secretParser = ({ from }: Dependencies['uuid']): Dependencies['secretParser'] => {
-//   return {
-//     toDomain: ({ _id, age, anonName, gender, likes, secret }) => {
-//       const id = _id.toString()
-//       return new Secret({
-//         id,
-//         age,
-//         anonName,
-//         gender,
-//         likes,
-//         secret
-//       })
-//     },
-//     toDocument: ({ id, age, anonName, gender, likes, secret }) => {
-//       const _id = from(id)
-//       return {
-//         _id,
-//         age,
-//         anonName,
-//         gender,
-//         likes,
-//         secret,
-//       }
-//     }
-//   }
-// }
-
 export class SecretParser {
   private _uuid: Dependencies['uuid']
 
-  constructor(uuid: Dependencies['uuid']) {
+  constructor({ uuid }: Pick<Dependencies, 'uuid'>) {
     this._uuid = uuid
   }
 
